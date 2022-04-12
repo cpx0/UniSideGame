db8e406b8fa2a96fa7c17fca4d656dce18bc1354
vision <vision2cpx@gmail.com>
2022-04-12 15:29:36 +0900

    Add public variable 'speed', transform player's direction

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rbody;      // Rigidbody2D 型の変数
-    float axisH = 0.0f;     // 入力
+    Rigidbody2D rbody;              // Rigidbody2D 型の変数
+    float axisH = 0.0f;             // 入力
+    public float speed = 3.0f;      // 移動速度
     
     // Start is called before the first frame update
     void Start()
@@ -19,11 +20,24 @@ public class PlayerController : MonoBehaviour
     {
         // 水平方向の入力をチェックする
         axisH = Input.GetAxisRaw("Horizontal");
+        // 向きの調整
+        if (axisH > 0.0f)
+        {
+            // 右移動
+            Debug.Log(" 右移動 ");
+            transform.localScale = new Vector2(1, 1);
+        }
+        else if (axisH < 0.0f)
+        {
+            // 左移動
+            Debug.Log(" 左移動 ");
+            transform.localScale = new Vector2(-1, 1); // 左右反転させる
+        }
     }
     
     void FixedUpdate()
     {
         // 速度を更新する
-        rbody.velocity = new Vector2(axisH * 3.0f, rbody.velocity.y);
+        rbody.velocity = new Vector2(speed * axisH, rbody.velocity.y);
     }
 }
